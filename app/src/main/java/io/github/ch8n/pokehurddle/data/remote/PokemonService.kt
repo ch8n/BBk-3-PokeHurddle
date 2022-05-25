705c31e3b3ff0869c63a17788608c073f8b9ce04
ch8n <chetan.garg36@gmail.com>
2022-05-25 21:16:58 +0530

    added comments and code clean up

--- app/src/main/java/io/github/ch8n/pokehurddle/data/remote/PokemonService.kt
+++ app/src/main/java/io/github/ch8n/pokehurddle/data/remote/PokemonService.kt
@@ -3,29 +3,48 @@ package io.github.ch8n.pokehurddle.data.remote
 import android.util.Log
 import com.google.gson.Gson
 import io.github.ch8n.pokehurddle.data.models.Pokemon
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import javax.inject.Inject
 import javax.inject.Singleton
 
+// one instance in dependency graph
 @Singleton
 class PokemonService @Inject constructor(
+    //👇 okhttp client is used for doing api calls
     private val okHttpClient: OkHttpClient
 ) {
 
-    fun fetchPokemon(id: Int): Pokemon? {
+    // fetch pokemon with ID
+    suspend fun fetchPokemon(id: Int): Pokemon? = withContext(Dispatchers.IO) {
+        // this scope runs over IO dispatcher not on main dispatcher.
+
+        // 👇 api url where we will fetch pokemon details from
+        val endpoint = "https://pokeapi.co/api/v2/pokemon/$id"
+
+        //👇 okHttp request builder for making api call
         val pokemonRequest = Request.Builder()
-            .url("https://pokeapi.co/api/v2/pokemon/$id")
+            .url(endpoint)
             .build()
+
         val response = kotlin.runCatching {
+            //👇 use okhttp client to do new request
             val data = okHttpClient.newCall(pokemonRequest).execute()
+            //👇 get response body as string
             val jsonString = data.body?.string()
+            //👇 convert jsonString to Pokemon instance
             Gson().fromJson(jsonString, Pokemon::class.java)
         }
+
+        // 👇 we will get pokemon instance if api call went well or..
         val pokemonDTO = response.getOrElse {
+            // 👇 ...for any error this section is executed
             Log.e("Error", "Pokemon fetch failed", it)
             null
         }
-        return pokemonDTO
+        // 👇 we return nullable response from this function
+        pokemonDTO
     }
 }
--- app/src/main/java/io/github/ch8n/pokehurddle/data/repository/Repository.kt
+++ app/src/main/java/io/github/ch8n/pokehurddle/data/repository/Repository.kt
@@ -5,7 +5,7 @@ import io.github.ch8n.pokehurddle.data.local.sources.PokemonDAO
 import io.github.ch8n.pokehurddle.data.models.Player
 import io.github.ch8n.pokehurddle.data.models.Pokemon
 import io.github.ch8n.pokehurddle.data.remote.PokemonService
-import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
@@ -13,31 +13,47 @@ import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
+
+// one instance in dependency graph
 @Singleton
-class AppRepository @Inject constructor(
+class PokemonRepository @Inject constructor(
+    //👇 pokemon service allows to fetch remote pokemon
     private val pokemonService: PokemonService,
+    //👇 pokemon DAO allows save and read pokemon from database
     private val pokemonDAO: PokemonDAO,
-    private val playerDAO: PlayerDAO,
-    private val ioDispatcher: CoroutineDispatcher
 ) {
-    suspend fun fetchPokemon(id: Int) = withContext(ioDispatcher) {
+
+    //👇 fetch pokemon of given id from remote server
+    suspend fun fetchPokemon(id: Int) = withContext(Dispatchers.IO) {
         pokemonService.fetchPokemon(id)
     }
 
-    suspend fun getPokemon(id: Int) = withContext(ioDispatcher) {
+    //👇 read pokemon of given id from database
+    suspend fun getPokemon(id: Int) = withContext(Dispatchers.IO) {
         pokemonDAO.getPokemon(id)
     }
 
-    suspend fun savePokemon(pokemon: Pokemon) = withContext(ioDispatcher) {
+    //👇 save pokemon of given id into the database
+    suspend fun savePokemon(pokemon: Pokemon) = withContext(Dispatchers.IO) {
         pokemonDAO.savePokemon(pokemon)
     }
+}
 
+// one instance in dependency graph
+@Singleton
+class PlayerRepository @Inject constructor(
+    //👇 player DAO allows us read/write player stats to database
+    private val playerDAO: PlayerDAO
+) {
+
+    //👇 get player details in observable type
     fun getPlayer(): Flow<Player> = playerDAO.getPlayer().map {
+        // if player is null then return empty player
         it.firstOrNull() ?: Player.Empty
-    }.flowOn(ioDispatcher)
+    }.flowOn(Dispatchers.IO)
 
-    suspend fun savePlayer(player: Player) = withContext(ioDispatcher) {
+    //👇 save player details in database
+    suspend fun savePlayer(player: Player) = withContext(Dispatchers.IO) {
         playerDAO.savePlayer(player)
     }
-
 }
