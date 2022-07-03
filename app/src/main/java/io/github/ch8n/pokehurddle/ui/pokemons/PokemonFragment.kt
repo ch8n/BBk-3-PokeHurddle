8b5a5781c9e8e196b98fd7c34ad3ca128a6e9db8
ch8n <chetan.garg36@gmail.com>
2022-07-03 14:15:58 +0530

    refactored

--- app/src/main/java/io/github/ch8n/pokehurddle/ui/pokemons/PokemonFragment.kt
+++ app/src/main/java/io/github/ch8n/pokehurddle/ui/pokemons/PokemonFragment.kt
@@ -15,19 +15,27 @@ import kotlinx.coroutines.flow.collect
 @AndroidEntryPoint
 class PokemonFragment : ViewBindingFragment<FragmentPokemonBinding>() {
 
+    // get shared view model
     private val viewModel: MainViewModel by activityViewModels()
 
     override fun setup(): Unit = with(binding) {
-        val gridAdapter = PokemonGridAdapter.newInstance()
+        // create grid adapter
+        val gridAdapter = PokemonGridAdapter()
+        // apply layout manager
         gridPokemon.layoutManager = GridLayoutManager(requireContext(), 2)
+        // apply grid adapter to recycler view
         gridPokemon.adapter = gridAdapter
+        // onResumed lifecycle
         lifecycleScope.launchWhenResumed {
+            // collect player stats
             viewModel.playerStats.collect {
+                // pass pokemon object into grid adapter
                 gridAdapter.submitList(it.pokemons)
             }
         }
     }
 
+    // bind layout to fragment
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentPokemonBinding
         get() = FragmentPokemonBinding::inflate
 
--- app/src/main/java/io/github/ch8n/pokehurddle/ui/pokemons/PokemonGridAdapter.kt
+++ app/src/main/java/io/github/ch8n/pokehurddle/ui/pokemons/PokemonGridAdapter.kt
@@ -2,47 +2,49 @@ package io.github.ch8n.pokehurddle.ui.pokemons
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import io.github.ch8n.pokehurddle.data.models.Pokemon
 import io.github.ch8n.pokehurddle.databinding.ListCellPokemonBinding
 
-class PokemonGridAdapter private constructor(
-    diffUtil: DiffUtil.ItemCallback<Pokemon>
-) : ListAdapter<Pokemon, PokemonVH>(diffUtil) {
+class PokemonGridAdapter() : RecyclerView.Adapter<PokemonVH>() {
 
+    // list of pokemon
+    private val currentList = mutableListOf<Pokemon>()
+
+    // update pokemon list in recycler view
+    fun submitList(pokemon: List<Pokemon>) {
+        currentList.clear()
+        currentList.addAll(pokemon)
+        notifyDataSetChanged()
+    }
+
+    // inflate UI for pokemon and bind to Pokemon view holder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonVH {
         val binding = ListCellPokemonBinding
             .inflate(LayoutInflater.from(parent.context), parent, false)
         return PokemonVH(binding)
     }
 
+    // bind list data to view holder
     override fun onBindViewHolder(holder: PokemonVH, position: Int) {
-        holder.onBind(getItem(position))
+        holder.onBind(currentList[position])
     }
 
-    companion object {
-        private val diffUtil = object : DiffUtil.ItemCallback<Pokemon>() {
-            override fun areItemsTheSame(oldItem: Pokemon, newItem: Pokemon): Boolean =
-                oldItem.id == newItem.id
-
-            override fun areContentsTheSame(oldItem: Pokemon, newItem: Pokemon): Boolean =
-                oldItem == newItem
-        }
-
-        fun newInstance() = PokemonGridAdapter(diffUtil)
-    }
+    // items in recycler view
+    override fun getItemCount(): Int = currentList.size
 }
 
+// Recycler View Holder for Pokemon
 class PokemonVH(private val binding: ListCellPokemonBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
     fun onBind(pokemon: Pokemon) = with(binding) {
+        // set pokemon image
         Glide.with(root.context)
             .load(pokemon.imageUrl)
             .into(imgPokemon)
+        // set Pokemon name
         labelPokemon.text = pokemon.name
     }
 }
